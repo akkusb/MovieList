01052aa2f9da4244238464423dd3ff590f29dc8b
Burhan Akkuş <akkusb@gmail.com>
2019-01-13 20:53:24 +0300

    Remote config fixed

--- MovieList/MovieList/AppDelegate.swift
+++ MovieList/MovieList/AppDelegate.swift
@@ -16,11 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         // Override point for customization after application launch.
         window?.rootViewController = SplashViewController()
-        
-        FirebaseApp.configure()
-        
+
         return true
     }
 
